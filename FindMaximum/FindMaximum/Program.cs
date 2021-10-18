5480e278a1fbd9014a310a8bfe1b6ed9394fe21a
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-18 14:05:28 +0530

    Welcome to find maximum problem

---
+++ FindMaximum/FindMaximum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindMaximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
